e2f42198245d3d0944e50610688d4c12297dc8a8
Assad Isah <nottherealalanturing@gmail.com>
2021-04-30 23:44:28 +0100

    v 1.3 - Fixing Color Mode

--- root-wrapper.js
+++ root-wrapper.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { ChakraProvider } from "@chakra-ui/react"
 import Layout from "./src/components/layout"
+import theme from "./theme"
 
 export const wrapPageElement = ({ element }) => {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={theme}>
       <Layout>{element}</Layout>
     </ChakraProvider>
   )
